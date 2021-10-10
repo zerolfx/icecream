e147cc399b14d9dafff3cbcf6c0411b2e5b0c9aa
zerolfx <zerolfx0@gmail.com>
2021-10-10 12:23:36 -0400

    minor updates

--- players/g1_player.py
+++ players/g1_player.py
@@ -1,9 +1,15 @@
+import functools
 import logging
 import math
 from typing import Callable, Dict, List, Tuple, Union, NamedTuple
 
 import numpy as np
 from collections import defaultdict
+
+TOTAL_UNITS = 24
+# test weighted = 0.01, 0.1, 0.5, 0.8, 1 and find the performance of 0.5 is the best
+# although I don't know why, but I guess we can test it for several rounds later
+WEIGHT = 0.5
 
 
 class Choice(NamedTuple):
@@ -33,43 +39,32 @@ def generate_choices(top_layer: np.ndarray, curr_level: np.ndarray) -> List[Choi
 
 def choose_next_player(now_turn: int, possible_players: List[int], served_situation: List[Dict[int, int]],
                        top_layer: np.ndarray, curr_level: np.ndarray, rng: np.random.Generator,
-                       group_id: int, my_flavor_preference: List[int]) -> int:
-    if len(possible_players) == 1:
-        return possible_players[0]
-
+                       my_flavor_preference: List[int]) -> int:
     # if now_turn = 0, it means for the first turn, we just randomly choose a person to pass to
     if now_turn == 0 and len(possible_players) != 0:
         return rng.choice(possible_players)
 
-    # if there are no candidates, which means our team is the last one to serve
-    if len(possible_players) == 0:
-        possible_players = [i for i in range(10) if i != group_id]
-
     choices = generate_choices(top_layer, curr_level)
-    # if all ice cream has been taken
-    if len(choices) == 0:
+
+    # if all ice cream has been taken, only happen in the last turn of last round
+    if not choices:
         return rng.choice(list(possible_players))
 
-    greedy_flavor_preference = defaultdict(list)
-    for player_index in possible_players:
-        player_served_situation = sorted(served_situation[player_index].items(), key=lambda item: -item[1])
-        for flavor, _ in player_served_situation:
-            greedy_flavor_preference[player_index].append(flavor)
+    greedy_flavor_preference = list()
+    for served_state in served_situation:
+        greedy_flavor_preference.append(sorted(served_state.keys(), key=lambda k: -served_state[k]))
 
     max_score = -math.inf
-    next_player_list = []
-    total_units = 24
-    # test weighted = 0.01, 0.1, 0.5, 0.8, 1 and find the performance of 0.5 is the best
-    # although I don't know why, but I guess we can test it for several rounds later
-    weighted = 0.5
+    next_player = None
 
-    for player_index, flavor_preference in greedy_flavor_preference.items():
+    for player_index in possible_players:
+        flavor_preference = greedy_flavor_preference[player_index]
         score_list = []
         for choice in choices:
             score_list.append((score(choice, flavor_preference), len(choice.flavors)))
 
         score_list.sort(key=lambda x: -x[0])
-        remain = total_units
+        remain = TOTAL_UNITS
         player_max_score = 0
         for score_num, count in score_list:
             if remain - count < 0:
@@ -77,30 +72,22 @@ def choose_next_player(now_turn: int, possible_players: List[int], served_situat
             remain -= count
             player_max_score += score_num
 
-        player_max_score = player_max_score / (total_units - remain) \
-                           + weighted * difference(my_flavor_preference, flavor_preference)
+        player_max_score = player_max_score / (TOTAL_UNITS - remain) + WEIGHT * difference(my_flavor_preference, flavor_preference)
 
         if max_score < player_max_score:
             max_score = player_max_score
-            next_player_list = [player_index]
-        elif max_score == player_max_score:
-            next_player_list.append(player_index)
+            next_player = player_index
 
-    # if len(next_player_list) == 1, just return,
-    # if the length is larger than 1, we may choose randomly choose a from the set
-    if len(next_player_list) == 1:
-        return list(next_player_list)[0]
-    return rng.choice(list(next_player_list))
+    return next_player
 
 
-# copy from the below f function
 def score(choice: Choice, flavor_preference) -> float:
     res = 0
     for flavor in choice.flavors:
         res -= flavor_preference.index(flavor)
     res /= len(choice.flavors)
     res += choice.max_depth * 0.2
-    res += 0.01 * len(choice.flavors)
+    res -= 0.01 * len(choice.flavors)
     return res
 
 
@@ -118,8 +105,6 @@ class Player:
         self.rng = rng
         self.logger = logger
         self.state = [0]
-        group_id = 1
-        self.group_id = group_id - 1
 
     def serve(self, top_layer: np.ndarray, curr_level: np.ndarray, player_idx: int,
               get_flavors: Callable[[], List[int]], get_player_count: Callable[[], int],
@@ -131,32 +116,22 @@ class Player:
         self.logger.info(choices)
         if not choices:
             turns = get_turns_received()
-            players = list()
-            for idx, turn in enumerate(turns):
-                if turn == min(turns) and idx != self.group_id:
-                    players.append(idx)
-            self.state.append(0)
+
+            next_players = list()
+            if len(set(turns)) == 1:  # last turn for current round
+                next_players = [i for i in range(len(turns)) if i != player_idx]
+            else:
+                for idx, turn in enumerate(turns):
+                    if turn == min(turns) and idx != player_idx:
+                        next_players.append(idx)
+
             # if we choose the person with the highest score
-            next_player = choose_next_player(min(turns), players, get_served(), top_layer, curr_level, self.rng,
-                                             self.group_id, self.flavor_preference)
+            next_player = choose_next_player(len(self.state), next_players, get_served(), top_layer, curr_level, self.rng,
+                                             self.flavor_preference)
+
+            self.state.append(0)
             return dict(action="pass", values=next_player)
 
-            # if we just randomly choose one person
-            # if len(players) == 0:
-            #     players = [i for i in range(10) if i != self.group_id]
-            # return dict(action="pass", values=self.rng.choice(players))
-
-        def f(choice: Choice) -> float:
-            res = 0
-            for flavor in choice.flavors:
-                res -= self.flavor_preference.index(flavor)
-            res /= len(choice.flavors)
-            # it seems that max_depth does have a positive impact on scoring after testing,
-            # but we should still test weight = 0.2
-            res += choice.max_depth * 0.2
-            res += 0.01 * len(choice.flavors)
-            return res
-
-        choice = max(choices, key=f)
+        choice = max(choices, key=functools.partial(score, flavor_preference=self.flavor_preference))
         self.state[-1] += len(choice.flavors)
         return dict(action='scoop', values=choice.index)
